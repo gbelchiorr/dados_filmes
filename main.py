fc855e6cbd12c9c57a64d3d55f0c38d65bf7f069
Guilherme Belchior <guibelchiorrb@gmail.com>
2022-07-20 18:11:37 -0300

    firstcommit

---
+++ main.py
@@ -0,0 +1,13 @@
+filme = input("Qual filme você quer saber sobre? ")
+
+filmes = {
+    "Os Vingadores": "Data de lançamento: 27 de abril de 2012 \nDuração do filme: 2h 23m \nBilheteria: 2,798 bilhões USD",
+    "Matrix":"Data de lançamento: 21 de maio de 1999 \nDuração do filme: 2h 16m \nBilheteria: 466,6 milhões USD",
+    "Marley & Eu":"Data de lançamento: 25 de dezembro de 2008 \nDuração do filme: 1h 55m \nBilheteria: 247,8 milhões USD",
+    "Batman O Cavaleiro das Trevas":"Data de lançamento: 18 de julho dee 2008 \nDuração do filme: 2h 32m \nBilheteria: 1,006 bilhões USD",
+    "Titanic":"Data de lançamento: 16 de janeiro de 1998 \nDuração do filme: 3h 14m \nBilheteria: 2,202 bilhões USD",
+    "Até que a Sorte Nos Separe":"Data de lançamento: 5 de outrubro de 2012 \nDuração do filme: 1h 44m \nBilheteria: R$ 34.565,441",
+    "American Pie":"Data de lançamento: 9 de julho de 1999 \nDuração do filme: 1h 35m \nBilheteria: 235.483.004 USD",
+    "Invocação do mal 3: A Ordem do Demônio":"Data de lançamento: 3 de junho de 2021 \nDuração do filme: 1h 52m \nBilheteria: 206,4 milhões USD"
+}
+print(filmes[filme]) 
